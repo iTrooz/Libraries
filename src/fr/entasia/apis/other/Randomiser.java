1945edd9c4dcb98e08213229f515d1b17ef2d52d
iTrooz_ <team.mortelgames@gmail.com>
2020-05-30 17:01:52 +0200

     

--- src/fr/entasia/apis/other/Randomiser.java
+++ src/fr/entasia/apis/other/Randomiser.java
@@ -1,42 +1,81 @@
 package fr.entasia.apis.other;
 
 import fr.entasia.errors.EntasiaException;
+import it.unimi.dsi.fastutil.doubles.Double2BooleanLinkedOpenHashMap;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Randomiser {
 
 	public static Random random = new Random();
-	public ArrayList<Integer> list = new ArrayList<>();
-	private boolean finalized = false;
 
-	public Randomiser add(int index, int percent){
-		list.add(index, percent);
-		return this;
+	private final int max;
+	private double r;
+	private double cPercent = 0;
+
+	public Randomiser(){
+		this(1000);
 	}
 
-	public Randomiser finish(){
-		int n = 0;
-		for (Integer integer : list) n += integer;
-		if(n!=100)throw new EntasiaException("Not 100%");
-		finalized = true;
-		return this;
+	public Randomiser(int max){
+		this.max = max;
+		this.r = random.nextInt(max);
 	}
 
-	public int random(){
-		if(finalized){
-			int price = random.nextInt(100);
-			int n = 0;
-			int i = 0;
-			for (Integer integer : list) {
-				n += integer;
-				if (price < n) {
-					return i;
-				}
-				i++;
-			}
-			return -10;
-		}else throw new EntasiaException("Not finalized");
+
+	public boolean isInNext(double percents){
+		if(cPercent ==-1)throw new EntasiaException("Value already found !");
+		cPercent +=percents;
+		if(cPercent >= max)throw new EntasiaException("Excedded maximum !");
+
+		if(r<cPercent){
+			cPercent = -1;
+			return true;
+		}
+		return false;
 	}
+
+	public double getNumber(){
+		return r;
+
+	}
+	public double getCPercent(){
+		return cPercent;
+	}
+
+
+
+
+//	private ArrayList<Integer> list = new ArrayList<>();
+//	private boolean finalized = false;
+
+//	public Randomiser add(int index, int percent){
+//		if(finalized)throw new EntasiaException("Already initialized !");
+//		list.add(index, percent);
+//		return this;
+//	}
+//
+//	public Randomiser finish(){
+//		int n = 0;
+//		for (Integer integer : list) n += integer;
+//		if(n!=100)throw new EntasiaException("Not 100%");
+//		finalized = true;
+//		return this;
+//	}
+//
+//	public int random(){
+//		if(finalized){
+//			int price = random.nextInt(100);
+//			int n = 0;
+//			int i = 0;
+//			for (Integer integer : list) {
+//				n += integer;
+//				if (price < n) {
+//					return i;
+//				}
+//				i++;
+//			}
+//			return -10;
+//		}else throw new EntasiaException("Not finalized");
+//	}
 }
