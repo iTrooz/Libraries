7280632c89d5d7ea9865d68af8dd85990f12037d
iTrooz_ <team.mortelgames@gmail.com>
2020-06-24 14:09:21 +0200

    added Mutable and Pair

---
+++ src/fr/entasia/apis/other/Mutable.java
@@ -0,0 +1,13 @@
+package fr.entasia.apis.other;
+
+public class Mutable <T> {
+
+    public T value;
+
+    public Mutable(){
+    }
+
+    public Mutable(T value){
+        this.value = value;
+    }
+}
---
+++ src/fr/entasia/apis/other/Pair.java
@@ -0,0 +1,15 @@
+package fr.entasia.apis.other;
+
+public class Pair<A, B> {
+
+    public A a;
+    public B b;
+
+    public Pair(){
+
+    }
+
+    public Pair(A a, B b){
+
+    }
+}
