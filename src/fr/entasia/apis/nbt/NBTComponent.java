99e637c3bd8a8d211bd412df8610016fff721d8f
iTrooz <itrooz.entasia@gmail.com>
2020-07-02 21:00:27 +0200

    added safe methods to NBTComponent

--- src/fr/entasia/apis/nbt/NBTComponent.java
+++ src/fr/entasia/apis/nbt/NBTComponent.java
@@ -94,6 +94,12 @@ public class NBTComponent {
 		return null;
 	}
 
+	public String getKeyStringSafe(String key) {
+		String a = getKeyString(key);
+		if(a==null)return "";
+		else return a;
+	}
+
 
 
 //	public void setList(String key) {
@@ -124,6 +130,12 @@ public class NBTComponent {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	public NBTComponent getComponentSafe(String key) {
+		NBTComponent a = getComponent(key);
+		if(a==null)return new NBTComponent();
+		else return a;
 	}
 
 	public String toString(){
