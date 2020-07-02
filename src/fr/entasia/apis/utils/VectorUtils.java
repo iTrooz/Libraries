3b0019ab4d11a47af689ea3be3dd3f54c62faa3a
iTrooz_ <team.mortelgames@gmail.com>
2020-07-02 11:14:32 +0200

    fixes

--- src/fr/entasia/apis/utils/VectorUtils.java
+++ src/fr/entasia/apis/utils/VectorUtils.java
@@ -2,7 +2,7 @@ package fr.entasia.apis.utils;
 
 import org.bukkit.util.Vector;
 
-public class OtherUtils {
+public class VectorUtils {
 
 	public static void limitVector(Vector v, double max){
 		if(v.getX()>max)v.setX(max);
@@ -14,5 +14,15 @@ public class OtherUtils {
 		if(v.getZ()>max)v.setZ(max);
 		else if(v.getZ()<-max)v.setZ(-max);
 	}
-	
+
+//	public static void scaleVector(Vector v, double max){
+//		if(v.getX()>max)v.setX(max);
+//		else if(v.getX()<-max)v.setX(-max);
+//
+//		if(v.getY()>max)v.setY(max);
+//		else if(v.getY()<-max)v.setY(-max);
+//
+//		if(v.getZ()>max)v.setZ(max);
+//		else if(v.getZ()<-max)v.setZ(-max);
+//	}
 }
