1312df5272936b3568aab08f4e35d2c335003137
iTrooz_ <team.mortelgames@gmail.com>
2020-08-16 15:15:08 +0200

    added PlayerUtils#fakeKill

--- src/fr/entasia/apis/utils/PlayerUtils.java
+++ src/fr/entasia/apis/utils/PlayerUtils.java
@@ -1,9 +1,13 @@
 package fr.entasia.apis.utils;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.util.Vector;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Locale;
 import java.util.UUID;
 
 public class PlayerUtils {
@@ -34,5 +38,26 @@ public class PlayerUtils {
 
 	public static UUID getUUID(String name){
 		return UUID.nameUUIDFromBytes(("OfflinePlayer:"+name).getBytes());
+	}
+
+	// warning : les fire ticks peuvent demander une BukkitRunnable#runTask();
+
+	public static void fakeKill(Player p){
+
+		for (PotionEffect pe : p.getActivePotionEffects()) {
+			p.removePotionEffect(pe.getType());
+		}
+
+		p.setMaxHealth(20);
+		p.setHealth(20);
+		p.setFoodLevel(20);
+
+		p.setSprinting(false);
+		p.setFallDistance(0);
+
+		p.setGliding(false);
+		p.setVelocity(new Vector(0, 0, 0));
+
+		p.setFireTicks(0);
 	}
 }
