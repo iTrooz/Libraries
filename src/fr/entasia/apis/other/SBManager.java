31c3b345c66f9edd3fcee401e68aefae35eddd9b
itrooz <team.mortelgames@gmail.com>
2020-09-02 18:59:53 +0200

    added SBManager API

---
+++ src/fr/entasia/apis/other/SBManager.java
@@ -0,0 +1,47 @@
+package fr.entasia.apis.other;
+
+import fr.entasia.errors.LibraryException;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+
+import java.util.Arrays;
+
+public abstract class SBManager {
+
+    public final Player p;
+    public Scoreboard scoreboard;
+    public Objective objective;
+
+    public String[] cache = new String[15];
+
+    public SBManager(String name, Player p){
+        this.p = p;
+        scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        objective = scoreboard.registerNewObjective(name, "dummy");
+        Arrays.fill(cache, "");
+    }
+
+    public void softSet(){
+        if(p.getScoreboard()!=scoreboard)refresh();
+    }
+
+    public void clear(){
+        scoreboard.getEntries().forEach(a -> scoreboard.resetScores(a));
+    }
+
+    public void refresh(){
+        p.setScoreboard(scoreboard);
+        clear();
+        objective.getScore("§bplay.enta§7sia.fr").setScore(0);
+    }
+
+    public void refreshLine(int number, String text){
+        if(number>=cache.length)throw new LibraryException("Too high line number. Maximum : "+cache.length);
+        scoreboard.resetScores(cache[number]);
+        cache[number] = text;
+        objective.getScore(text).setScore(number);
+    }
+
+}
