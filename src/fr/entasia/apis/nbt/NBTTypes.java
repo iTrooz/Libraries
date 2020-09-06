3778ce363ace3b47b60e4c261ed97ff526f52291
itrooz <team.mortelgames@gmail.com>
2020-09-06 17:18:48 +0200

    added Boolean NBT type

--- src/fr/entasia/apis/nbt/NBTTypes.java
+++ src/fr/entasia/apis/nbt/NBTTypes.java
@@ -1,9 +1,11 @@
 package fr.entasia.apis.nbt;
 
+import net.minecraft.server.v1_15_R1.NBTTagCompound;
+
 import java.lang.reflect.Method;
 
 public enum NBTTypes {
-	String(String.class), Byte(byte.class), Short(short.class), Int(int.class), Long(long.class), Float(float.class), Double(double.class);
+	String(String.class), Byte(byte.class), Short(short.class), Int(int.class), Long(long.class), Float(float.class), Double(double.class), Boolean(boolean.class);
 
 	public Class<?> type;
 	public Method getter;
